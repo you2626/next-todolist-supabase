07b382a4cf769cc42554ae26e44788ba716ab4fd
KURUMI <k.1au12um1@gmail.com>
2024-10-07 07:23:35 +0900

    todo入力が反映

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -2,12 +2,13 @@
 
 import { Input, Textarea } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { getAllTodos } from "../utils/supabaseFunction";
+import { addTodo, getAllTodos } from "../utils/supabaseFunction";
 import TodoList from "./TodoList";
 
 const AddTask=()=>{
     
     const [todos,setTodos]=useState<any>([]);
+    const[title,setTitle]=useState<string>("");
 
     useEffect(()=>{
         const getTodos=async()=>{
@@ -18,14 +19,22 @@ const AddTask=()=>{
         getTodos();
     },[]);
 
+    const handleSubmit=async(e:any)=>{
+        e.preventDefault();
+        console.log("a")
+
+        await addTodo(title);
+        setTitle("");
+    };
+
     return(
         <section>
             <h3 className="text-2xl font-bold text-gray-700 py-5">TODO追加</h3>
-        <form>
+        <form onSubmit={(e)=>handleSubmit(e)}>
         <div className="grid w-full items-center gap-4 max-w-xl">
             <div className="flex flex-col space-y-1.5">
                 <label htmlFor="title">タイトル</label>
-                <Input type="text" placeholder="タイトルを入力" />
+                <Input type="text" placeholder="タイトルを入力" onChange={(e)=>setTitle(e.target.value)} />
             </div>
             <div className="flex flex-col space-y-1.5">
                 <label htmlFor="description">内容</label>
--- src/app/utils/supabaseFunction.ts
+++ src/app/utils/supabaseFunction.ts
@@ -4,3 +4,8 @@ export const getAllTodos=async()=>{
     const todos =await supabase.from("todo").select("*");
     return todos.data;
 };
+
+export const addTodo= async(title:string) =>{
+    console.log(title)
+    await supabase.from("todo").insert({title:title});
+};
